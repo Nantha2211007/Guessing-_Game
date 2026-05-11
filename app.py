99306e54ab2762f6aa3162c2704b0c66d7e6eba8
Nantha Kumar <nantha2211008@gmail.com>
2026-05-11 15:01:36 +0530

    Added requirements and final deployment setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def guess():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
